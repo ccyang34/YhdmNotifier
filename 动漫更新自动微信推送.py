a45570b791872a38a563ae5f8037d669e7ec7fd4
ccyang34 <100679967+ccyang34@users.noreply.github.com>
2024-08-02 21:26:56 +0800

    Update 动漫更新自动微信推送.py

--- 动漫更新自动微信推送.py
+++ 动漫更新自动微信推送.py
@@ -49,8 +49,9 @@ def get_anime_updates():
         if (title == "永生" or any(keyword in title for keyword in keywords)) and update_date == today:
             episode = item.select_one('a.names > span.ep_name').text.strip()
             link = 'https://yhdm.one' + item.select_one('a.names')['href']
-            updates.append(f"<font size=\"6\" color=\"red\"><a href=\"{link}\">{title}</a></font>\n{episode} 🔥\n更新日期：{update_date}\n---\n")  #  更新日期另起一行
+            updates.append(f"<font size=\"6\" color=\"red\"><a href=\"{link}\"><font color=\"red\">{title}</font></a></font>\n{episode} 🔥\n更新日期：{update_date}\n---\n")  #  更新日期另起一行
     return updates
+
 
 if __name__ == "__main__":
     updates = get_anime_updates()
