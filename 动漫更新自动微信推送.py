c606be730a43da86b521218afbc24b44efb81097
ccyang34 <100679967+ccyang34@users.noreply.github.com>
2024-08-02 23:55:54 +0800

    Update 动漫更新自动微信推送.py

--- 动漫更新自动微信推送.py
+++ 动漫更新自动微信推送.py
@@ -36,8 +36,8 @@ def get_anime_updates():
     soup = BeautifulSoup(response.text, 'html.parser')
 
     keywords = ["完美世界", "仙逆", "吞噬星空", "斗破苍穹", "斗罗大陆", "遮天", "武神主宰", "凡人修仙传", "诛仙"]
-    today = datetime.date.today().strftime("%Y-%m-%d")
-    yesterday = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    today = datetime.date.today()
+    valid_dates = [ (today - datetime.timedelta(days=i)).strftime("%Y-%m-%d") for i in range(7) ] # 获取过去一周的日期
 
     anime_items = soup.select('ul.latest-ul > li')
     updates = []
@@ -46,8 +46,8 @@ def get_anime_updates():
         title = item.select_one('a.names > span.name').text.strip()
         update_date = item.select_one('em').text.strip()
 
-        # 筛选标题和更新日期（今天或昨天）
-        if (title == "永生" or any(keyword in title for keyword in keywords)) and (update_date == today or update_date == yesterday):
+        # 筛选标题和更新日期（过去一周）
+        if (title == "永生" or any(keyword in title for keyword in keywords)) and update_date in valid_dates:
             episode = item.select_one('a.names > span.ep_name').text.strip()
             link = 'https://yhdm.one' + item.select_one('a.names')['href']
             updates.append(f"<font size=\"6\" color=\"red\"><a href=\"{link}\" style=\"color: red; text-decoration-color: red;\"><font color=\"red\">{title}</font></a></font>\n {episode} 🔥 更新日期：{update_date}\n\n")
