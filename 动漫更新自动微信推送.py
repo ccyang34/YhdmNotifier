bb0b533fff006c769c2b11b0828fc348b9063590
ccyang34 <100679967+ccyang34@users.noreply.github.com>
2024-08-11 11:57:40 +0800

    Update 动漫更新自动微信推送.py

--- 动漫更新自动微信推送.py
+++ 动漫更新自动微信推送.py
@@ -71,8 +71,9 @@ def get_anime_updates():
 if __name__ == "__main__":
     updates = get_anime_updates()
     if updates:
-        message = f"# 🔥 本周动漫更新 🔥\n\n" \
-                  f"**(优选线路GS,HN,WJ,WL,SN,JS,MD)**\n\n" + "".join(updates)
+        message = f"<center><span style=\"color: red; font-size: 24px\">🔥 本周动漫更新 🔥</span></center>\n\n" \
+                  f"<center><span style=\"font-size: 12px\">(优选线路GS,HN,WJ,WL,SN,JS,MD)</span></center>\n\n" \
+                  + "".join(updates)
 
         # 使用 topicId 群发消息
         response = send_message(message, topic_ids=TARGET_TOPIC_ID)
